21b9f485c118dbe748b8f10408c203a8c6701d35
David Gomez-Urquiza <david.gurquiza@gmail.com>
2013-06-26 00:07:52 -0500

    Each person has the right to take part in the management of public affairs in his country, provided he has prior experience, a will to succeed, a university degree, influential parents, good looks, a curriculum vitae, two 3x4 snapshots, and a good tax record.

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
 require 'sinatra'
 require 'open-uri'
 
-get '/v/:id.mp4' do
-  send_file File.expand_path("#{params[:id]}-backward.mp4")
+set :port, 80
+set :root, File.dirname(__FILE__)
+set :public_folder, "#{settings.root}/public"
+set :forwards, "#{settings.public_folder}/forwards"
+set :backwards, "#{settings.public_folder}/backwards"
+
+get '/' do
+  'go to /v/yourVineId'
 end
 
-get '/v/:id' do
-  id = params[:id]
+get '/v/:id.mp4' do
+  send_file File.join(settings.backwards,"#{params[:id]}.mp4")
+end
+
+get '/v/:id' do |id|
   url = "http://vine.co/v/#{id}"
   html = open(url).read
   video_url = html.match(/(https?:\/\/.*\.mp4)\?/)[1]
-  puts "curl -L #{video_url} -o #{id}.mp4"
-  `curl -L #{video_url} -o #{id}.mp4`
-  `bash reverse.sh #{id}.mp4 #{id}-backward.mp4`
-  subed = html.gsub(video_url, "http://localhost:4567/v/#{id}.mp4")
+  unless File.file?("#{settings.backwards}/#{id}.mp4")
+    # puts "curl -L #{video_url} -o #{settings.forwards}/#{id}.mp4"
+    `curl -L #{video_url} -o #{settings.forwards}/#{id}.mp4`
+    `bash reverse.sh #{settings.forwards}/#{id}.mp4 #{settings.backwards}/#{id}.mp4`
+  end
+  subed = html.gsub(video_url, "/v/#{id}.mp4")
   subed
 end
-
